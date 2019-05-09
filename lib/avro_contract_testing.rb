3e86503d1d851ae407142b626b025dd546a026c4
tabdollahi <tania.abdollahi@vts.com>
2019-05-09 11:39:14 -0400

    TA - PRODUCT-12435 - initial commit for creating gem using bundler.

---
+++ lib/avro_contract_testing.rb
@@ -0,0 +1,6 @@
+require "avro_contract_testing/version"
+
+module AvroContractTesting
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/avro_contract_testing/version.rb
@@ -0,0 +1,3 @@
+module AvroContractTesting
+  VERSION = "0.1.0"
+end
